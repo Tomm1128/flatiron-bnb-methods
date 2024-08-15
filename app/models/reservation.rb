93c6448330ea4d5c3b9abac1c026b425c9960a45
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-08-15 09:31:50 -0400

    Create reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,50 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :is_guest_host
+  validate :check_availability
+  validate :checkin_before_checkout
+  validate :checkin_and_checkout_not_same
+
+  def duration
+    checkout - checkin
+  end
+
+  def total_price
+    (checkout - checkin) * listing.price
+  end
+
+  private
+
+  def is_guest_host
+    if guest_id == listing.host_id
+      errors.add(:guest, "Guest and host can't be the same user.")
+    end
+  end
+
+  def check_availability
+    overlapping_reservations =
+      listing.reservations
+             .where.not(id: id)
+             .where("checkin < ? AND checkout > ?", checkout, checkin)
+
+    if overlapping_reservations.exists?
+      errors.add(:base, "Listing is not available for the selected dates.")
+    end
+  end
+
+  def checkin_before_checkout
+    return unless checkin && checkout
+    if checkin >= checkout
+      errors.add(:checkin, "must be before checkout.")
+    end
+  end
+
+  def checkin_and_checkout_not_same
+    return unless checkin && checkout
+    if checkin == checkout
+      errors.add(:base, "Checkin and checkout dates cannot be the same.")
+    end
+  end
 end
